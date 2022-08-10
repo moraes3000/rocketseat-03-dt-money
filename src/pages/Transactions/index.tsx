4a0bf2b55dbec08160f5407c638e0965f7701402
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-08-10 13:09:59 -0300

    Tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { PriceHighlight, TransactionContainer, TransactionTable } from './styles';
 
 
 export function Transaction() {
@@ -7,6 +8,26 @@ export function Transaction() {
     <div>
       <Header />
       <Summary />
+      <TransactionContainer>
+        <TransactionTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td><PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight></td>
+              <td>Venda</td>
+              <td>10/08/2022</td>
+            </tr>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td><PriceHighlight variant='outcome'>R$ 12.000,00</PriceHighlight></td>
+              <td>Venda</td>
+              <td>10/08/2022</td>
+            </tr>
+
+          </tbody>
+        </TransactionTable>
+      </TransactionContainer>
     </div>
+
   );
 };
