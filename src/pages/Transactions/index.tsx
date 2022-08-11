5ca00864cb622f6045467cc65932ab720e55c53c
Bruno Moraes <bruno_bmoraes@hotmail.com>
2022-08-11 11:05:10 -0300

    Realizando requisição HTTP

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from 'react';
+
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
 import { SearchForm } from './components/SearchForm';
+
 import { PriceHighlight, TransactionContainer, TransactionTable } from './styles';
 
+interface TransactionProps {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAd: string
+
+}
 
 export function Transaction() {
+  const [transactions, setTransactions] = useState<TransactionProps[]>([])
+
+  useEffect(() => {
+    async function loadTransaction() {
+      const api = 'http://localhost:3000/transactions/'
+      const response = await fetch(api)
+      const data = await response.json()
+
+      setTransactions(data)
+    }
+    loadTransaction()
+  }, [])
+
+
   return (
     <div>
       <Header />
@@ -13,12 +39,18 @@ export function Transaction() {
         <SearchForm />
         <TransactionTable>
           <tbody>
-            <tr>
-              <td width='50%'>Desenvolvimento de site</td>
-              <td><PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight></td>
-              <td>Venda</td>
-              <td>10/08/2022</td>
-            </tr>
+
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width='50%'>{transaction.description}</td>
+                  <td><PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight></td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.createdAd}</td>
+                </tr>
+              )
+            })}
+
             <tr>
               <td width='50%'>Desenvolvimento de site</td>
               <td><PriceHighlight variant='outcome'>R$ 12.000,00</PriceHighlight></td>
